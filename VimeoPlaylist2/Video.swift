602f6c9dfb3e62512fab56f84896aa3a0811bcc9
Michael Gordon <mike1gordon@gmail.com>
2015-08-09 18:33:23 +0200

    Converting JSON to Model

---
+++ VimeoPlaylist2/Video.swift
@@ -0,0 +1,21 @@
+//
+//  Video.swift
+//  VimeoPlaylist2
+//
+//  Created by Michael Gordon on 09/08/2015.
+//  Copyright (c) 2015 Personal. All rights reserved.
+//
+
+import Foundation
+class Video {
+    
+    var name: String = ""
+    var duration: Int = 0
+    var imageURLString: String = ""
+    
+    init(dictionary: Dictionary<String, AnyObject>) {
+        
+    }
+    
+    
+}
--- VimeoPlaylist2/VimeoClient.swift
+++ VimeoPlaylist2/VimeoClient.swift
@@ -5,9 +5,8 @@
 //  Created by Michael Gordon on 09/08/2015.
 //  Copyright (c) 2015 Personal. All rights reserved.
 //
-
 import Foundation
-typealias ServerResponseCallback = (object: Dictionary<String,AnyObject>?, error: NSError?) -> Void
+typealias ServerResponseCallback = (videos: Array<Video>?, error: NSError?) -> Void
 class VimeoClient {
     static let errorDomain = "VimeoClientErrorDomain"
     static let baseURLString = "https://api.vimeo.com"
@@ -21,7 +20,7 @@ class VimeoClient {
         
         if URL == nil {
             var error = NSError(domain: errorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Unable to create URL"])
-            callback(object: nil, error: error)
+            callback(videos: nil, error: error)
             return
         }
         
@@ -33,7 +32,7 @@ class VimeoClient {
             
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
                 if error != nil {
-                    callback(object: nil, error: error)
+                    callback(videos: nil, error: error)
                     return
                 }
                 
@@ -42,19 +41,34 @@ class VimeoClient {
                 
                 if let constJSONError = JSONError {
                     
-                    callback(object: nil, error: JSONError)
+                    callback(videos: nil, error: JSONError)
                     
                     return
                 }
                 
                 if JSON == nil {
                     var error = NSError(domain: self.errorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Unable to parse JSON"])
-                    callback(object: nil, error: error)
+                    callback(videos: nil, error: error)
                     
                     return
                 }
                 
-                callback(object: JSON, error: nil)
+                var videoArray = Array<Video>()
+                
+                if let constJSON = JSON {
+                    var dataArray = constJSON["data"] as? Array<Dictionary<String,AnyObject>>
+                    
+                    if let constArray = dataArray {
+                        
+                        for value in constArray {
+                            
+                            let video = Video(dictionary: value)
+                            videoArray.append(video)
+                        }
+                    }
+                }
+                
+                callback(videos: videoArray, error: nil)
             })
         })
         
