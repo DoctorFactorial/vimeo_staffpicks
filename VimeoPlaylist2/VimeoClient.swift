1ab290c9ba1853dacbd59b2f5f980f7c8fdafd41
Michael Gordon <mike1gordon@gmail.com>
2015-08-09 18:29:03 +0200

    Authentication Header

--- VimeoPlaylist2/VimeoClient.swift
+++ VimeoPlaylist2/VimeoClient.swift
@@ -7,9 +7,40 @@
 //
 
 import Foundation
+
 typealias ServerResponseCallback = (object: Dictionary<String,AnyObject>?, error: NSError?) -> Void
 class VimeoClient {
+    static let errorDomain = "VimeoClientErrorDomain"
+    static let baseURLString = "https://api.vimeo.com"
+    static let staffpicksPath = "/channels/staffpicks/videos"
+    static let authToken = "ce92421f6053cb824102f40ac1668471"
+    
     class func staffpicks(callback: ServerResponseCallback)  {
-        callback(object: nil, error: nil)
+        
+        let URLString = baseURLString + staffpicksPath
+        var URL = NSURL(string: URLString)
+        
+        if URL == nil {
+            var error = NSError(domain: errorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Unable to create URL"])
+            callback(object: nil, error: error)
+            return
+        }
+        
+        var request = NSMutableURLRequest(URL: URL!)
+        request.HTTPMethod = "GET"
+        request.addValue("Bearer " + authToken, forHTTPHeaderField: "Authorization")
+        
+        var task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+            
+            var responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+            println(response)
+            println(responseString)
+            
+            callback(object: nil, error: nil)
+            
+        })
+        
+        task.resume()
     }
 }
