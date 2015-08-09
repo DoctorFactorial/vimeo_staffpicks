037d2ba22ae1cdc2a1fcdacb32f9e9642748a1df
Michael Gordon <mike1gordon@gmail.com>
2015-08-09 17:30:24 +0200

    UI Table View Cell Subclass

--- VimeoPlaylist2/StaffPicksViewController.swift
+++ VimeoPlaylist2/StaffPicksViewController.swift
@@ -7,25 +7,24 @@
 //
 
 import UIKit
-
 class StaffPicksViewController: UIViewController, UITableViewDataSource {
     @IBOutlet var tableView: UITableView?
     
     let items = ["alfie", "lee", "mattan", "syd", "zach"]
     
-    // Mark: View Lifecycle
+    // MARK: View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        self.title = "Staff Picks"
         self.setupTableView()
     }
     
     // MARK: Setup
     
     func setupTableView() {
-        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: "placeholder")
+        self.tableView?.registerClass(VideoCell.self, forCellReuseIdentifier: NSStringFromClass(VideoCell.self))
     }
     
     // MARK: UITableViewDataSource
@@ -36,7 +35,7 @@ class StaffPicksViewController: UIViewController, UITableViewDataSource {
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("placeholder") as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(VideoCell.self)) as! VideoCell
         
         cell.textLabel?.text = self.items[indexPath.row]
         
---
+++ VimeoPlaylist2/VideoCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoCell.swift
+//  VimeoPlaylist2
+//
+//  Created by Michael Gordon on 09/08/2015.
+//  Copyright (c) 2015 Personal. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
