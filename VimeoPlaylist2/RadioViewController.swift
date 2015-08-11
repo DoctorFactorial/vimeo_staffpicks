21ed4003d94990debb81fdffe10eb951870f6bbd
Michael Gordon <mike1gordon@gmail.com>
2015-08-11 14:38:21 +0200

    Rock Radio added

--- VimeoPlaylist2/RadioViewController.swift
+++ VimeoPlaylist2/RadioViewController.swift
@@ -12,6 +12,8 @@ class RadioViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.navigationBarHidden = true
 
         // Do any additional setup after loading the view.
     }
@@ -21,15 +23,9 @@ class RadioViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapRock(sender: AnyObject)
+    {
+        var viewController = RockRadioViewController(nibName: "RockRadioViewController", bundle: nil)
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
-    */
-
 }
---
+++ VimeoPlaylist2/RockRadioViewController.swift
@@ -0,0 +1,74 @@
+//
+//  RockRadioViewController.swift
+//  VimeoPlaylist2
+//
+//  Created by Michael Gordon on 11/08/2015.
+//  Copyright (c) 2015 Personal. All rights reserved.
+//
+
+import UIKit
+
+class RockRadioViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView?
+    
+    var items: Array<Video> = []
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "Popular Tracks"
+        self.setupTableView()
+        self.refreshItems()
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden((false), animated: false)
+    }
+    
+    
+    // MARK: Setup
+    
+    func setupTableView() {
+        let nib = UINib(nibName: "VideoCell", bundle: nil)
+        self.tableView?.registerNib(nib, forCellReuseIdentifier: NSStringFromClass(VideoCell.self))
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(VideoCell.self)) as! VideoCell
+        
+        let video = self.items[indexPath.row]
+        cell.video = video
+        
+        return cell
+    }
+    
+    func refreshItems() {
+        RockRadioClient.popular {(videos, error) -> Void in
+            
+            if let constVideos = videos {
+                
+                for video: Video in constVideos {
+                    
+                    self.items = constVideos
+                    
+                    self.tableView?.reloadData()
+                }
+            }
+            else {
+                // TODO: alert the user
+            }
+        }
+    }
+}
