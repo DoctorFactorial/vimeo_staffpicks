c3188e84c06783ddd69188a0c2ca25451fda34c9
DoctorFactorial <hello@michaelgordon.io>
2015-09-01 14:05:05 +0200

    Buttons styled dynamically

--- VimeoPlaylist2/YourMusicViewController.swift
+++ VimeoPlaylist2/YourMusicViewController.swift
@@ -25,20 +25,36 @@ class YourMusicViewController: UIViewController {
         // Add the 3 Default buttons dynamically
         var buttonNames = ["AtWork", "Bike", "AtHome"]
         
-        for (index, value) in enumerate(buttonNames)
+        var playlists = [
+                ["name": "AtWork", "red": 0, "green": 165, "blue": 229],
+                ["name": "Bike", "red": 42, "green": 154, "blue": 42],
+                ["name": "AtHome", "red": 229, "green": 150, "blue": 0]
+        ]
+        
+        println(playlists[0]["name"])
+            
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let width = screenSize.width - 100
+        
+        for (index, value) in enumerate(playlists)
         {
-            var buttonName = buttonNames[index]
+            var playlist = playlists[index]
+            var buttonName = playlist["name"] as! String
             var actionName = buttonName + ":"
             
+            var red = CGFloat((playlist["red"] as! Float)/255)
+            var green = CGFloat((playlist["green"] as! Float)/255)
+            var blue = CGFloat((playlist["blue"] as! Float)/255)
+            
             var x = CGFloat(50)
-            var y = CGFloat(50 * index)
+            var y = CGFloat(200 + 66 * index)
             
             let myButton = UIButton()
             
             myButton.setTitle("\(buttonName)", forState: .Normal)
             myButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-            myButton.backgroundColor = UIColor.blueColor()
-            myButton.frame = CGRectMake(x, y, 300, 44)
+            myButton.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+            myButton.frame = CGRectMake(x, y, width, 44)
             myButton.addTarget(self, action: Selector(actionName), forControlEvents: .TouchUpInside)
             self.view.addSubview(myButton)
             
@@ -99,41 +115,41 @@ class YourMusicViewController: UIViewController {
 //        self.navigationController?.pushViewController(viewController, animated: true)
 //    }
     
-    func didTapAtWorkButton(sender: UIButton) {
-        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
-        // 3. After viewController definition before call, set playlists variable for use in other controller
-        viewController.playlists = self.items
-        viewController.index = 3
-        self.navigationController?.pushViewController(viewController, animated: true)
-        
-    }
-
-    @IBAction func didTapAtWork(sender: AnyObject) {
-        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
-        // 3. After viewController definition before call, set playlists variable for use in other controller
-        viewController.playlists = self.items
-        viewController.index = 0
-        self.navigationController?.pushViewController(viewController, animated: true)
-    
-    }
-    
-    @IBAction func didTapPlaylistOne(sender: AnyObject) {
-        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
-        // 3. Acter viewController definition before call, set playlists variable for use in other controller
-        viewController.playlists = self.items
-        viewController.index = 1
-        self.navigationController?.pushViewController(viewController, animated: true)
-        
-    }
-    
-    @IBAction func didTapPlaylistTwo(sender: AnyObject) {
-        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
-        // 3. Acter viewController definition before call, set playlists variable for use in other controller
-        viewController.playlists = self.items
-        viewController.index = 2
-        self.navigationController?.pushViewController(viewController, animated: true)
-        
-    }
+//    func didTapAtWorkButton(sender: UIButton) {
+//        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
+//        // 3. After viewController definition before call, set playlists variable for use in other controller
+//        viewController.playlists = self.items
+//        viewController.index = 3
+//        self.navigationController?.pushViewController(viewController, animated: true)
+//        
+//    }
+//
+//    @IBAction func didTapAtWork(sender: AnyObject) {
+//        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
+//        // 3. After viewController definition before call, set playlists variable for use in other controller
+//        viewController.playlists = self.items
+//        viewController.index = 0
+//        self.navigationController?.pushViewController(viewController, animated: true)
+//    
+//    }
+//    
+//    @IBAction func didTapPlaylistOne(sender: AnyObject) {
+//        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
+//        // 3. Acter viewController definition before call, set playlists variable for use in other controller
+//        viewController.playlists = self.items
+//        viewController.index = 1
+//        self.navigationController?.pushViewController(viewController, animated: true)
+//        
+//    }
+//    
+//    @IBAction func didTapPlaylistTwo(sender: AnyObject) {
+//        var viewController = PlaylistViewController(nibName: "PlaylistViewController", bundle: nil)
+//        // 3. Acter viewController definition before call, set playlists variable for use in other controller
+//        viewController.playlists = self.items
+//        viewController.index = 2
+//        self.navigationController?.pushViewController(viewController, animated: true)
+//        
+//    }
     
 //    @IBAction func didTapRadio(sender: AnyObject) {
 //        var viewController = RadioViewController(nibName: "RadioViewController", bundle: nil)
